7532f52fae22d3ac4b705f628ac6fd2f9b9ae4d0
istrategy <gertvaneeden@gmail.com>
2022-02-19 17:26:08 +0200

    Add write data to CSV

--- alpaca/generateBlocks.py
+++ alpaca/generateBlocks.py
@@ -8,8 +8,8 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 import json
-
-
+import os
+import csv
 
 class updateData:
     # filename = './data/JSE202202151d.csv'
@@ -25,21 +25,35 @@ class updateData:
 
     def setLabel(self, lastValue,lastPlus1):
         target = ((lastPlus1/lastValue)-1) *100
-        retValue ="No change"
+        retValue ="no_change"
         if target < -5:
-            retValue = 'down 5%'
+            retValue = 'down_5perc'
         if target < -2 and target > -5:
-            retValue = 'down 2-5%'
+            retValue = 'down_2-5perc'
         if target < 0 and target > -2:
-            retValue = ' down 0-2%'
+            retValue = 'down_0-2perc'
         if target > 0 and target < 2:
-            retValue = 'up 0-2%'
+            retValue = 'up_0-2perc'
         if target > 2 and target < 5:
-            retValue = 'up 2-5%'
+            retValue = 'up_2-5perc'
         if target > 5:
-            retValue = 'up 5%'
+            retValue = 'up_5perc'
         return retValue, target
 
+    def saveCSV(self, target,targetString,block):
+        folder = "./data/"+target[2]
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        targetDate = target[0].strftime("%Y-%m-%d")
+        tfilename = targetString[0]  + "__" + targetDate +"__" + str(target[3]) +"_LABEL_"+targetString[1][0]+ '.csv'
+        filename =  folder + tfilename
+        # print(filename)
+        with open(filename, 'w', newline='', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            for item in block:
+                # print(item)
+                writer.writerow(item)
 
     def generateBlocks(self, shareArray, fromDate, toDate, blocksize):
         rowcounter =1
@@ -82,13 +96,17 @@ class updateData:
                     mycursor.execute(itemSql)
                     myresult = mycursor.fetchall()
                     for target in myresult:
+                        targetString = [str(fromDate), self.setLabel(lastValue,target[3]),target[2],lastValue, target[3]]
                         targetArray.append([str(fromDate), self.setLabel(lastValue,target[3]),target[2],lastValue, target[3]])
                         newStartDate = str(target[0])
+                        # self.saveCSV(target,targetString,row)
                         # print(targetArray)
+
                 shareCounter += 1
 
             if exitFunc == False:
                 batchArray.append(block)
+                self.saveCSV(target, targetString, block)
             # fromDate = str(lastDate)
             fromDate = str(newStartDate)
 
@@ -114,8 +132,8 @@ class updateData:
 
 updaeObj = updateData()
 
-startDate = "2010-01-01"
-startDate = "2021-12-01"
+# startDate = "2010-01-01"
+startDate = "2021-11-01"
 # endDate = date.today()
 endDate = '2022-01-01'
 # print(startDate, endDate)
@@ -123,4 +141,4 @@ shareArray=['SLM.JO','ABG.JO','SOL.JO','TKG.JO']
 
 blocksize = 10
 
-print(updaeObj.generateBlocks(shareArray, startDate, endDate, blocksize))
+updaeObj.generateBlocks(shareArray, startDate, endDate, blocksize)
